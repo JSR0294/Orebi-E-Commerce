2ead473a7ffb803236bb6fdd861d1c251981b973
JSR0294 <jsrwebdebloper@gmail.com>
2023-12-20 19:49:16 +0600

    small prob solved

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import InputField from "./layouts/InputField";
-import Btn from './layouts/Btn'
 
 const ContactForm = () => {
   return (
     <>
       <h3 className=" text-primaryColor font-bold text-4xl">Fill up a Form</h3>
-      <div className="mt-10">
+      <form className="mt-10">
         <InputField
           label={"Name"}
           placeholder={"Your name here"}
           type={"text"}
+          id={'full name'}
           className={"border-b border-[#F0F0F0] w-[750px]"}
           inputClassName={"mt-3 mb-4"}
           labelClassName={"text-primaryColor font-bold text-base"}
@@ -19,6 +19,7 @@ const ContactForm = () => {
           label={"Email"}
           placeholder={"Your email here"}
           type={"email"}
+          id={'email'}
           className={"border-b border-[#F0F0F0] w-[750px]"}
           inputClassName={"mt-3 mb-4"}
           labelClassName={"text-primaryColor font-bold text-base mt-6"}
@@ -27,12 +28,13 @@ const ContactForm = () => {
           label={"Massage"}
           placeholder={"Massage type here"}
           type={"text"}
+          id={'message'}
           className={"border-b border-[#F0F0F0] w-[750px]"}
           inputClassName={"mt-3 mb-4"}
           labelClassName={"text-primaryColor font-bold text-base mt-6"}
         ><textarea className=" resize-none h-[115px] w-full" ></textarea></InputField>
-      </div>
-      <Btn btnName={'Post'} className={"py-4 px-20 bg-primaryColor text-white font-bold text-sm "} btnClassName={'mt-8'} link={''}/>
+      <InputField type={'submit'} value={'Post'} className={"py-4 px-20 bg-primaryColor text-white font-bold text-sm mt-8 w-[200px] cursor-pointer"} inputClassName={'cursor-pointer'}></InputField>
+      </form>
     </>
   );
 };
--- src/components/layouts/InputField.jsx
+++ src/components/layouts/InputField.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const InputField = ({className, labelClassName, label, type, placeholder, inputClassName, children}) => {
+const InputField = ({className, labelClassName, label, type, id, value, placeholder, inputClassName, children}) => {
   return (
-    <form action="" className={className}>
+    <div className={className}>
         <label htmlFor="" className={`block ${labelClassName}`}>{label}</label>
-        <input type={type} placeholder={placeholder} className={`border-none outline-none ${inputClassName}`} />
+        <input type={type} id={id} value={value} placeholder={placeholder} className={`border-none outline-none ${inputClassName}`} />
         {children}
-    </form>
+    </div>
   )
 }
 
